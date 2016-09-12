5b925b16f01da9ef5043697dbf03191e947f150d
luqsthunder <lucasthund3r@gmail.com>
2016-09-12 08:17:33 -0300

    solving sintax errors of fragment shader

--- Shaders/simpleshader/shader.frag.glsl
+++ Shaders/simpleshader/shader.frag.glsl
@@ -1,12 +1,11 @@
 #version 330 core
-
 in vec2 texCoord;
 
-//uniform Sample2D uTexture;
+uniform sampler2D uTexture;
 
 out vec4 color;
 
 void main()
 {
-  color = vec4(0.8, 0.0, 1.0, 1.0);//texture(uTexture, texCoord);
+  color = texture(uTexture, texCoord);
 }
