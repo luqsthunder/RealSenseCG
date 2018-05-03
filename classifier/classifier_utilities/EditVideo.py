74db716e42da43f978a275066e440395275df339
lucas amaral <lucasthund3r@gmail.com>
2018-05-03 17:02:54 -0300

    edits on video classifier and mode how aquire images

--- classifier/classifier_utilities/EditVideo.py
+++ classifier/classifier_utilities/EditVideo.py
@@ -3,9 +3,9 @@ import cv2 as cv
 import os
 
 
-folder_src = 'C:/Users/lucas/Documents/Projects/realsensecg/Gestures/dynamic_poses/uncuted'
-folder_dest = 'C:/Users/lucas/Documents/Projects/realsensecg/Gestures/dynamic_poses/DB'
-folder_trash_dest = 'C:/Users/lucas/Documents/Projects/realsensecg/Gestures/dynamic_poses/uncuted'
+folder_src = '../../Gestures/dynamic_poses/uncut/norm_depth'
+folder_dest = '../../Gestures/dynamic_poses/DB'
+folder_trash_dest = '../../Gestures/dynamic_poses/trash'
 
 cv.namedWindow('video')
 cv.namedWindow('currentCut')
@@ -14,7 +14,7 @@ curr_first = 0
 length_show = 10
 curr_last = curr_first + length_show
 
-#video information
+# video information
 current_gesture = 0  # PX
 current_video = 0    # EX
 current_video_frame_cont = length_show
@@ -23,66 +23,118 @@ current_video_frame_cont = length_show
 show_top = []
 show_botton = []
 
+cut_pieces = []
+trash_cut_pieces = []
+curr_video_show = []
+
+initial_cut = 0
+
+font = cv.FONT_HERSHEY_SIMPLEX
+font_scale = 0.5
+line_type = 2
+fps = 0
+range_video = curr_last - curr_first
+window_frame = np.array((10, 10))
+
 for idx, uncut_vid_path in enumerate(os.listdir(folder_src)):
-    uncuted_video_folder = sorted(os.listdir(os.path.join(folder_src,
-                                                          uncut_vid_path)),
-                                  key=len)
-    uncuted_video_folder = [os.path.join(folder_src, uncut_vid_path, l) for l in uncuted_video_folder]
-    size_total = len(uncuted_video_folder)
+    cut_pieces.append([])
+    uncut_video_folder = sorted(os.listdir(os.path.join(folder_src,
+                                                        uncut_vid_path)),
+                                key=len)
+    uncut_video_folder = [os.path.join(folder_src, uncut_vid_path, l)
+                          for l in uncut_video_folder]
+    size_total = len(uncut_video_folder)
+    if size_total == 0:
+        continue
 
     cutting = True
+    cut = False
     open_cut = False
 
-    for idx_rng in range(0, 5):
-        show_top.append(cv.imread(uncuted_video_folder[idx_rng]))
-    for idx_rng in range(5, 10):
-        show_botton.append(cv.imread(uncuted_video_folder[idx_rng]))
+    show_top.clear()
+    show_botton.clear()
 
     loading = True
     while cutting:
 
         if loading:
+            show_top.clear()
+            show_botton.clear()
+
+            range_video = curr_last - curr_first
+            fps = 0
+            curr_video_show.clear()
+            for idx_rng in range(curr_first, curr_last):
+                curr_video_show.append(cv.imread(uncut_video_folder[idx_rng]))
+
+            for idx_rng in range(curr_first, curr_first + 5):
+                if idx_rng < curr_last:
+                    show_top.append(cv.imread(uncut_video_folder[idx_rng]))
+                else:
+                    show_top.append(np.zeros((640, 480, 3)))
+
+            for idx_rng in range(curr_last - 5, curr_last):
+                if curr_first < idx_rng:
+                    show_botton.append(cv.imread(uncut_video_folder[idx_rng]))
+                else:
+                    show_botton.append(np.zeros((640, 480, 3)))
+
             top_img = show_top[0]
             for idx_top_img in range(1, len(show_top)):
-                top_img = np.concatenate((top_img, show_top[idx_top_img]), axis=1)
+                top_img = np.concatenate((top_img, show_top[idx_top_img]),
+                                         axis=1)
 
             bot_img = show_botton[0]
             for idx_top_img in range(1, len(show_top)):
-                bot_img = np.concatenate((bot_img, show_botton[idx_top_img]), axis=1)
+                bot_img = np.concatenate((bot_img, show_botton[idx_top_img]),
+                                         axis=1)
 
-            windowFrame = np.concatenate((top_img, bot_img), axis=0)
-            windowFrame = cv.resize(windowFrame, (1366, 768))
+            window_frame = np.concatenate((top_img, bot_img), axis=0)
+            window_frame = cv.resize(window_frame, (1366, 768))
             loading = False
 
+        cv.putText(window_frame,
+                   'First Frame to cut is: {}'.format(curr_first), (0, 25),
+                   font, font_scale, (0, 255, 0), line_type)
+        cv.putText(window_frame,
+                   'Last Frame to cut is: {}'.format(curr_last), (0, 50),
+                   font, font_scale, (0, 255, 0), line_type)
+        cv.putText(window_frame,
+                   'amount of images: {}'.format(size_total), (0, 75),
+                   font, font_scale, (0, 255, 0), line_type)
 
-        #cv.putText(windowFrame, (0, 25),
-        #           'First Frame to cut begin: {}'.format(curr_first),
-        #           cv.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0))
+        cv.imshow('video', window_frame)
 
-        cv.imshow('video', windowFrame)
-        key = cv.waitKey(0)
+        cv.imshow('currentCut', curr_video_show[fps])
+        fps = (fps + 1) % range_video
 
-        #posição de cima
+        key = cv.waitKey(60)
+
+#       posição de cima
         if key == ord('w'):
-            curr_first = curr_first + 1 if curr_first < size_total \
-                                        else curr_first
+            curr_first = max(initial_cut, min(curr_first + 1, curr_last))
+            loading = True
         elif key == ord('s'):
-            curr_first = curr_first - 1 if curr_first < size_total \
-                                        else 0
+            curr_first = max(initial_cut, min(curr_first - 1, curr_last))
+            loading = True
 
-        #posição de baixo
-        if key == ord('a'):
-            curr_last = curr_last + 1 if curr_last < size_total \
-                                      else curr_last
-        elif key == ord('d'):
-            curr_last = curr_last - 1 if curr_last < size_total \
-                                      else 0
-
-        if key == ord(' '):
-            open_cut = True
+#       posição de baixo
+        if key == ord('d'):
+            curr_last = max(curr_first + 1, min(curr_last + 1, size_total - 1))
+            loading = True
+        elif key == ord('a'):
+            curr_last = max(curr_first + 1, min(curr_last - 1, size_total - 1))
+            loading = True
 
         if key == ord('q'):
             break
 
+        if cut:
+            cut_pieces[idx].append((curr_first, curr_last))
+            curr_last = initial_cut
+            loading = False
+            cut = False
+
+cv.destroyAllWindows()
 
 
--- src/main.cc
+++ src/main.cc
@@ -42,46 +42,9 @@
 #include "rect.h"
 #include "graphprobs.h"
 
-uint16_t maxDepth = 3000;
-
-#define MAXLINE 500000
-
-
-template <typename T>
-T 
-clamp(const T& n, const T& lower, const T& upper) {
-  return std::max(lower, std::min(n, upper));
-}
-
-void
-toOCV(const std::vector<uint16_t> &in,
-      cv::Mat &out) {
-  cv::Mat out1{640, 480, CV_16UC1};
-  for(size_t y = 0; y < 480; ++y) {
-    for(size_t x = 0; x < 640; ++x) {
-      out1.at<uint16_t>(x, y) = in[x + y * 640];
-    }
-  }
-  out = out1.clone();
-}
-
-typedef cv::Point3_<uint8_t> Pixel;
-
-void
-toOCVColor(const std::vector<uint8_t> &in, cv::Mat &out) {
-  for(size_t y = 0; y < 640; ++y) {
-    for(size_t x = 0; x < 480; ++x) {
-      out.at<Pixel>(x, y) = Pixel{in[3 * (x * 640 + y)], 
-                                  in[3 * (x * 640 + y) + 1], 
-                                  in[3 * (x * 640 + y) + 2]};
-    }
-  }
-}
-
 std::vector<float>
 classifyImgNet(sf::TcpSocket &sock, const std::vector<int> &im,
                cv::Size imSize) {
-  char okStr[5];
   size_t reciLen;
   char predictBuff[50000];
 
@@ -167,99 +130,82 @@ findAndCutBoundingBoxFromImage(cv::Mat& imOut) {
 }
 
 
-bool opencvSaveVideoFromFrames(const std::vector<cv::Mat>& imgs,
-                               int seqNum, int folderNum,
-                               std::string testOrTrain, int clsNum) {
+size_t
+amountImgUncutClsFolder(int clsNum, const std::string &name) {
   namespace fs = boost::filesystem;
-  std::string fileName = "Gestures/dynamic_poses/F" +
-                          std::to_string(folderNum) + "/" +
-                          testOrTrain + "/P" + std::to_string(clsNum) 
-                          + "/e" + std::to_string(seqNum) + ".avi";
+  std::string folder = "Gestures/dynamic_poses/uncut/" + name + "/P" +
+                        std::to_string(clsNum) + "/";
 
-  fs::path p(fileName);
-  if(fs::exists(fileName) && !fs::is_directory(fileName)) {
-    fs::remove(p);
-  }
+  return (size_t) std::count_if(
+    fs::directory_iterator(folder),
+    fs::directory_iterator(),
+    static_cast<bool(*)(const fs::path&)>(fs::is_regular_file));
+}
 
-  cv::VideoWriter vWriter;
-  vWriter.open(fileName, CV_FOURCC_DEFAULT, 60.0, {640, 480});
-  for(const auto &it : imgs) {
-    cv::Mat aux;
-    cv::cvtColor(it, aux, CV_GRAY2BGR);
-    vWriter.write(aux);
+
+bool 
+saveSingleImage(const cv::Mat &imCDepth, const cv::Mat &im100, 
+                const cv::Mat &imNormTotal, int imNum, int clsNum) {
+  namespace fs = boost::filesystem;
+
+  std::string folder = "Gestures/dynamic_poses/uncut/complete_depth/P" +
+                        std::to_string(clsNum) + "/";
+  if(!fs::is_directory(folder)) {
+    fs::path p(folder);
+    fs::create_directory(p);
   }
-  vWriter.release();
+  cv::imwrite(folder + "imCDp" + std::to_string(imNum) + ".ppm", imCDepth);
+
+
+  folder = "Gestures/dynamic_poses/uncut/100x100/P" +
+                        std::to_string(clsNum) + "/";
+  if(!fs::is_directory(folder)) {
+    fs::path p(folder);
+    fs::create_directory(p);
+  }
+  cv::imwrite(folder + "im100" + std::to_string(imNum) + ".png", im100);
+
+
+  folder = "Gestures/dynamic_poses/uncut/norm_depth/P" +
+                        std::to_string(clsNum) + "/";
+  if(!fs::is_directory(folder)) {
+    fs::path p(folder);
+    fs::create_directory(p);
+  }
+  cv::imwrite(folder + "imNDp" + std::to_string(imNum) + ".png", imNormTotal);
 
   return true;
 }
 
-bool opencvSaveFramesToSequence(const std::vector<cv::Mat>& imgs,
-                                int seqNum, int folderNum,
-                                std::string testOrTrain, int clsNum) {
-    namespace fs = boost::filesystem;
-    std::string folderName = "Gestures/dynamic_poses/F" +
-                              std::to_string(folderNum) + "/" +
-                              testOrTrain +
-                              "/P" + std::to_string(clsNum) +
-                              "/e" + std::to_string(seqNum) + "/";
-
-    fs::path p(folderName);
-    if(fs::is_directory(folderName)) {
-      for(fs::directory_iterator end_dir_it, it(p);
-          it != end_dir_it; ++it) {
-        fs::remove_all(it->path());
-      }
-    }
-    else {
-      fs::create_directory(p);
-    }
-
-    for(int i = 0; i < imgs.size(); ++i) {
-      cv::imwrite(folderName + "im" + std::to_string(i) + ".jpg",
-                  imgs[i]);
-    }
-
-    return true;
-}
 
 int
 main(int argc, char **argv)
 {
-  uint16_t currMax = maxDepth, normValue = 0;
   auto device = rscg::CameraDeviceRSWindows();
   rscg::GraphProbs graph{200};
 
   bool setToStop = false;
 
-  cv::Mat frame          {480, 640, CV_8UC1,  cv::Scalar(0)},
-          frameColor     {480, 640, CV_8UC3,  cv::Scalar(0)},
-          dist           {480, 640, CV_16UC1, cv::Scalar(0)},
-          distColor      {480, 640, CV_8UC3,  cv::Scalar(0)},
-          frame2WithRect {480, 640, CV_8UC3,  cv::Scalar(0)},
-          color          {480, 640, CV_8UC3,  cv::Scalar(0)}, 
-          frame2         {480, 640, CV_8UC1,  cv::Scalar(0)},
-          frame2Color    {480, 640, CV_8UC3,  cv::Scalar(0)},
-          graphIm        {480, 640, CV_8UC3,  cv::Scalar(0)},
-          fDepth         {480, 640, CV_8UC1,  cv::Scalar(0)},
-          im5050         { 50,  50, CV_8UC1,  cv::Scalar(0)},
-          im5050Color    { 50,  50, CV_8UC3,  cv::Scalar(0)},
-          fDepthColor    {512, 424, CV_8UC1,  cv::Scalar(0)};
+  cv::Mat frame                   {480, 640, CV_8UC1  ,  cv::Scalar(0)},
+          frameColor              {480, 640, CV_8UC3  ,  cv::Scalar(0)},
+          frame16C                {480, 640, CV_16UC1 ,  cv::Scalar(0)},
+          frameNormDepthRng       {480, 640, CV_8UC1  ,  cv::Scalar(0)},
+          frameNormDepthRngColor  {480, 640, CV_8UC3  ,  cv::Scalar(0)},
+          graphIm                 {480, 640, CV_8UC3  ,  cv::Scalar(0)},
+          im100x100               {100, 100, CV_8UC1  ,  cv::Scalar(0)},
+          im100x100Color          {100, 100, CV_8UC3  ,  cv::Scalar(0)};
 
   int thresh = 200;
-
-  double min, max;
-  cv::Point min_loc, max_loc;
 
   cv::namedWindow("frame", 0);
   cv::createTrackbar("thresh", "frame", &thresh, 1000);
 
-  std::vector<uint16_t> imDepth;
-  std::vector<uint8_t> imColor;
-
   std::vector<int> imToClassify;
   imToClassify.resize(50 * 50 * 3);
 
-  uint16_t value = 0;
+  uint16_t value = 0, depthCloserCam = 9999;
+  const uint16_t maxDepth = 32727;
+  uint8_t normValue = 0;
 
   sf::TcpSocket sock;
   bool connected = true;
@@ -271,67 +217,47 @@ main(int argc, char **argv)
 
   std::vector<float> resClass;
 
-  int cont = 0;
-  rscg::Rect<int> boundingBox;
+  size_t cont = 0;
 
   bool recording = false;
 
-  int contFiles = 0;
-
-  cv::Mat h;
-  cv::Mat fullIm{480*2, 640*3, CV_8UC3, cv::Scalar(0)};
-
-  sf::Clock clk;
-  clk.restart();
-
-  std::vector<cv::Mat> seqIm;
-  seqIm.reserve(1000);
+  cv::Mat fullIm{480 * 2, 640 * 3, CV_8UC3, cv::Scalar(0)};
 
   int fontFace = cv::FONT_HERSHEY_SCRIPT_SIMPLEX;
   double fontScale = 1;
   int thickness = 3;
   int baseline;
 
-  std::future<bool> saveSeqFut;
-  int currSeqNum = 0;
-  int folderNum = 1;
   int clsNum = 1;
-  uint32_t imsInSeq = 0;
-  std::string testOrTrain = "train";
-
-  std::vector<std::string> clsNames{"y", "oi"};
 
   bool isSavingVideos = false;
 
   while(!setToStop || recording) {
     device.fetchDepthFrame();
 
-    currMax = maxDepth;
-
-    uint16_t currMinDepth = 9999;
-    for(const auto &it : imDepth) {
-      if(it != 0 && it < currMinDepth) {
-        currMinDepth = it;
+    // find the closest depth from camera
+    depthCloserCam = 9999;
+    for(const auto &it : device.getDepthFrame1Chanels()) {
+      if(it != 0 && it < depthCloserCam) {
+        depthCloserCam = it;
       }
     }
 
     for(size_t y = 0; y < 640; ++y) {
       for(size_t x = 0; x < 480; ++x) {
         value = device.getDepthFrame1Chanels()[x * 640 + y];
-        if(maxDepth < value) {
-          maxDepth = value;
-        }
 
-        normValue = (uint8_t)((255.00) * ((double)value / (double)maxDepth));
+        frame.at<uint8_t>(x, y) = value % 255;
+        frame16C.at<uint16_t>(x, y) = 
+          ((value < (uint16_t)thresh + depthCloserCam) && (value > 10) ?
+            value % 255 : 0);
 
-        frame.at<uint8_t>(x, y) =
-          (uint8_t)(( (value < (uint16_t)thresh + currMinDepth)
-                    && (value > 10)) ? 255 : 0);
+        // normalizing using linear interpolation
+        // normValue = (uint8_t)((255.00) * ((double)value / (double)maxDepth));
 
-        frame2.at<uint8_t>(x, y) = (((value < (uint16_t)thresh + currMinDepth)
-                                     && (value > 10)) ? value : 0);
-        fDepth.at<uint8_t>(x, y) = normValue;
-
+        frameNormDepthRng.at<uint8_t>(x, y) =
+          ( (value < (uint16_t)thresh + depthCloserCam) ? 
+              value % 255: 0);
       }
     }
     
@@ -339,109 +265,59 @@ main(int argc, char **argv)
     if(reskey == 'q') {
       setToStop = true;
     }
-
-    else if(!recording && reskey == 't') {
-      testOrTrain = (testOrTrain == "train" ? "test" : "train");
-    }
-    else if(reskey == 'f') {
-      ++folderNum;
-    }
-    else if(reskey == 'g') {
-      --folderNum;
-    }
     else if(reskey == 'c') {
-      currSeqNum = 0;
       ++clsNum;
+      cont = amountImgUncutClsFolder(clsNum, "norm_depth");
     }
     else if(reskey == 'v') {
-      currSeqNum = 0;
       --clsNum;
-    }
-    else if(reskey == 'a') {
-      isSavingVideos = !isSavingVideos;
+      cont = amountImgUncutClsFolder(clsNum, "norm_depth");
     }
     else if(reskey == ' ') {
       recording = !recording;
-      if(!recording) {
-        if(!isSavingVideos) {
-          saveSeqFut = std::async(std::launch::async,
-                                  // function launch async
-                                  opencvSaveFramesToSequence,
-                                  // parameters to function
-                                  seqIm, currSeqNum, folderNum, testOrTrain,
-                                  clsNum);
-        }
-        else {
-          saveSeqFut = std::async(std::launch::async,
-                                  // function to launch async
-                                  opencvSaveVideoFromFrames,
-                                  // parameters to function
-                                  seqIm, currSeqNum, folderNum, testOrTrain,
-                                  clsNum);
-        }
-        cont = 0;
-        ++currSeqNum;
-      }
-      else {
-        seqIm.clear();
-      }
     }
 
-    auto cir = frame;
-
-    if(recording)
-      cv::circle(cir, max_loc, 10, cv::Scalar(128));
-    if(boundingBox.h != -1 && connected) {
-      resClass = classifyImgNet(sock, imToClassify, {50, 50});
-      graph.update(resClass);
-      graphIm = graph.render();
-    }
-
-    cv::resize(frame2, im5050, cv::Size{50, 50}, 0, 0, CV_INTER_AREA);
+    cv::resize(frameNormDepthRng, im100x100, cv::Size{100, 100}, 0, 0, 
+               CV_INTER_AREA);
 
     cv::cvtColor(frame, frameColor, cv::COLOR_GRAY2BGR);
-    frameColor.copyTo(fullIm(cv::Rect(640, 0, frameColor.cols, 
-                      frameColor.rows)));
+    frameColor.copyTo(fullIm(cv::Rect(0, 0, frameColor.cols, 
+                                      frameColor.rows)));
+    cv::cvtColor(frameNormDepthRng, frameNormDepthRngColor, 
+                 cv::COLOR_GRAY2BGR);
+    frameNormDepthRngColor.copyTo(fullIm(cv::Rect(640, 0, 
+                                         frameNormDepthRngColor.cols,
+                                         frameColor.rows)));
 
-    cv::cvtColor(fDepth, fDepthColor, cv::COLOR_GRAY2BGR);
-    fDepthColor.copyTo(fullIm(cv::Rect(0, 0, frameColor.cols,
-                               frameColor.rows)));
-    
-    cv::cvtColor(im5050, im5050Color, cv::COLOR_GRAY2BGR);
-    im5050Color.copyTo(fullIm(cv::Rect(2*640, 0, im5050Color.cols, 
-                                       im5050Color.rows)));
+    cv::cvtColor(im100x100, im100x100Color, cv::COLOR_GRAY2BGR);
+    im100x100Color.copyTo(fullIm(cv::Rect(2*640, 0, im100x100Color.cols, 
+                                       im100x100Color.rows)));
                                        
     graphIm.copyTo(fullIm(cv::Rect(0, 480, graphIm.cols,
                                    graphIm.rows)));
 
     cv::Size textSize = cv::getTextSize(std::to_string(cont), fontFace,
                                         fontScale, thickness, &baseline);
-    cv::putText(fullIm, std::to_string(cont), {0, 20}, fontFace, fontScale,
+    cv::putText(fullIm, std::to_string(cont), {0, 25}, fontFace, fontScale,
                 cv::Scalar(0, 128, 0), 1, 1);
-
-    textSize = cv::getTextSize(std::string("Folder Num:") +
-                               std::to_string(folderNum), fontFace, fontScale, 
-                               thickness, &baseline);
-    cv::putText(fullIm, std::string("Folder Num:") + std::to_string(folderNum), 
-                {0, 100}, fontFace, fontScale, cv::Scalar(0, 128, 0), 1, 1);
 
     textSize = cv::getTextSize(std::string("cls name:")
                                + std::to_string(clsNum), fontFace, fontScale,
                                thickness, &baseline);
     cv::putText(fullIm, std::string("cls name:") + std::to_string(clsNum),
-                {0, 200}, fontFace, fontScale, cv::Scalar(0, 128, 0), 1, 1);
+                {0, 75}, fontFace, fontScale, cv::Scalar(0, 128, 0), 1, 1);
 
     textSize = cv::getTextSize(std::string("curr seq:")
-                               + std::to_string(currSeqNum), fontFace, fontScale,
+                               + std::to_string(cont), fontFace, fontScale,
                                thickness, &baseline);
-    cv::putText(fullIm, std::string("curr seq:") + std::to_string(currSeqNum),
-                {0, 300}, fontFace, fontScale, cv::Scalar(0, 128, 0), 1, 1);
+    cv::putText(fullIm, std::string("curr seq:") + std::to_string(cont),
+                {0, 125}, fontFace, fontScale, cv::Scalar(0, 128, 0), 1, 1);
 
     cv::imshow("frame", fullIm);
     
-    if(recording)
-    {
-      seqIm.push_back(frame2.clone());
+    if(recording) {
+      std::async(std::launch::async, saveSingleImage, 
+                 frame16C, im100x100, frameNormDepthRng, cont, clsNum);
       cont++;
     }
   }
